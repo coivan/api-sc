d37124c87325868cfa25d5c3c2c8d40e8bfa1a2d
Ivan Cardoso <cardoso.ivan2012@gmail.com>
2020-03-26 10:07:30 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/ico/apisalvacents/ApisalvacentsApplication.java
@@ -0,0 +1,13 @@
+package com.ico.apisalvacents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisalvacentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisalvacentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ico/apisalvacents/ApisalvacentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ico.apisalvacents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisalvacentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
