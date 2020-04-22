4e432544770eddcf5b45778bea5f482e654c805c
Ivan Cardoso <cardoso.ivan2012@gmail.com>
2020-04-22 14:42:27 -0300

    Obtem um post por id

---
+++ src/main/java/com/ico/apisalvacents/services/PostService.java
@@ -0,0 +1,24 @@
+package com.ico.apisalvacents.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ico.apisalvacents.domain.Post;
+import com.ico.apisalvacents.repository.PostRepository;
+import com.ico.apisalvacents.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Obejeto não encontrado")); 
+		
+	}
+}
