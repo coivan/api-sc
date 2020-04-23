2275f0cbbc207fa1274900fb7a633ee70ba29aaf
Ivan Cardoso <cardoso.ivan2012@gmail.com>
2020-04-23 11:57:48 -0300

    Adiciona comentários aos posts

--- src/main/java/com/ico/apisalvacents/config/Instantiation.java
+++ src/main/java/com/ico/apisalvacents/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.ico.apisalvacents.domain.Post;
 import com.ico.apisalvacents.domain.User;
 import com.ico.apisalvacents.dto.AuthorDTO;
+import com.ico.apisalvacents.dto.CommentDTO;
 import com.ico.apisalvacents.repository.PostRepository;
 import com.ico.apisalvacents.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo.", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite as férias", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Bom dia!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
